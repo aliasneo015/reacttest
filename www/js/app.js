97f9ee5aaf8c00d5764602ac7236430052fa8913
Kewal K <ramya.kewal@gmail.com>
2015-07-16 19:48:28 -0700

    adding angular and nodejs pulled modules

--- www/js/app.js
+++ www/js/app.js
@@ -4,13 +4,14 @@ requirejs.config({
   baseUrl: 'js/lib',
   paths: {
     app: '../app',
-    jquery: 'jquery-2.1.1'
+    jquery: 'jquery-2.1.1',
+    JSXTransformer: 'JSXTransformer-0.13.3'
   }
 });
 
 //start main app
 requirejs(
-  ['jquery', 'backbone', 'react', 'underscore'],
+  ['jquery', 'backbone', 'react', 'JSXTransformer', 'underscore', 'angular'],
   function () {
       requirejs(['app/main']);
   }
--- www/js/app/main.js
+++ www/js/app/main.js
@@ -1,9 +1,20 @@
 define(function (require) {
 
-  var $ = require('jquery');
+  var $ = require('jquery'),
+      React = require('react'),
+      JSXTransformer = require('JSXTransformer')
 
   $(function() {
-    alert("main loaded!!");
+
+
+ 
   });
+
+    alert("main loaded!!");
+//     React.render (
+//      <h1> Hello World! </h1>
+//      document.getElementById('example')
+//    );
+
 
 });
